5fc346dfc47c40f570f794ed43619dcc215cbf02
Harish Boke <harishboke@Harishs-MacBook-Pro.local>
2021-05-23 01:20:28 +0530

    Es6 for old browsers with parcel

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import "./styles.scss";
+console.log("hello world!");
+
+
+async function getPosts() {
+	const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+	const posts = await response.json();
+
+	const res = posts.map( post => (`<li>${post.title}</li>`))
+	document.getElementById('posts').innerHTML = res
+}
+
+ function init() {
+		getPosts()
+}
+
+init()
